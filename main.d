028f6a5fe4d655c598e6b5c5e509c647f60b086d
Daniel Bugl <Daniel@touchlay.com>
2012-12-30 22:19:50 +0100

    Added README and more Makefile examples + Makefile templates.

--- main.d
+++
@@ -1,4 +0,0 @@
-main.o: main.cpp arduino/Arduino.h arduino/binary.h arduino/WCharacter.h \
-  arduino/WString.h arduino/HardwareSerial.h arduino/Stream.h \
-  arduino/Print.h arduino/Printable.h arduino/new.h \
-  arduino/variants/standard/pins_arduino.h
